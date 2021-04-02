f22e96f4f551b140bf779df5fbfc6efdecc36470
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-04-01 20:35:33 -0600

    Add to user create method for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   def new; end
 
   def create
+    user = User.new(user_params)
     redirect_to '/profile'
+  end
+
+  private
+  def user_params
+    params.permit(:email, :username, :first_name, :last_name, :password)
   end
 end
