cf03c6dd5a0e655c350d9bc637591e4d44683c91
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-04-01 19:53:32 -0600

    Add spec for when fields are empty

--- spec/features/users/new_user_spec.rb
+++ spec/features/users/new_user_spec.rb
@@ -18,4 +18,20 @@ RSpec.describe 'User Sign Up' do
     expect(page).to have_content("ryan21")
     expect(page).to have_content("Ryan Laleh, You are now signed up!")
   end
+
+  it "user sign up fails, Fields empty" do
+    visit '/sign_up'
+
+    fill_in :email, with: ""
+    fill_in :username, with: ""
+    fill_in :first_name, with: "Ryan"
+    fill_in :last_name, with: "Laleh"
+    fill_in :password, with: "1234Abc!"
+    fill_in :confirm_password, with: "1234Abc!"
+
+    click_button 'Submit'
+
+    expect(current_path).to eq('/sign_up')
+    expect(page).to have_content("User could not be created: [\"Email can't be blank\", \"Username can't be blank\"]")
+  end
 end
