554be81d417620144ce0da7e28da48f7a0acb497
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-04-03 20:59:03 -0600

    change route to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'home#index'
   #users
   get '/sign_up', to: 'users#new'
   post '/users', to: 'users#create'
