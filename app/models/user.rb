92b53205162b843bcf3e292f4e0858c35528741b
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-04-04 20:20:27 -0600

    trying to figure out devise for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   validates_presence_of :first_name
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: { case_sensitive: true }, on: :create
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
 
   has_secure_password
 
