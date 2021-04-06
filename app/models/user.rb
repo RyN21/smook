b0953008541ecd75725b80bccd545044d9f3310b
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-04-05 21:53:57 -0600

    Reset database and migrations to recreate users table with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates_presence_of :first_name
-  validates :email, presence: true, uniqueness: true
-  validates :username, presence: true, uniqueness: true
-  validates :password, presence: true, confirmation: { case_sensitive: true }, on: :create
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
-
-  has_secure_password
-
-  def name
-    if !last_name.nil?
-      first_name.concat(" #{last_name}")
-    else
-      first_name
-    end
-  end
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable
 end
