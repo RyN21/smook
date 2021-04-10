6d55c3e4cfbb6e8ecb88d3d024e5b43846885a4c
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-04-09 23:00:38 -0600

    Working on the forms and tests

--- spec/features/users/new_user_spec.rb
+++ spec/features/users/new_user_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'User Sign Up' do
   it 'user can be created and sign up' do
-    visit '/sign_up'
-
-    fill_in :email, with: "test1@test.com"
-    fill_in :username, with: "ryan21"
-    fill_in :first_name, with: "Ryan"
-    fill_in :last_name, with: "Laleh"
-    fill_in :password, with: "1234Abc!"
-    fill_in :password_confirmation, with: "1234Abc!"
+    visit '/users/sign_up'
+    save_and_open_page
+    fill_in :Email, with: "laleh21@yahoo.com"
+    fill_in :Username, with: "ryn21"
+    fill_in :Password, with: "1234Abc!"
+    fill_in :Password_confirmation, with: "1234Abc!"
 
     click_button 'Submit'
     expect(current_path).to eq('/profile')
@@ -100,3 +98,27 @@ RSpec.describe 'User Sign Up' do
   #   expect(page).to have_content("User could not be created: [\"Password must consist of at least 8 characters, and.. \"]")
   # end
 end
+
+
+#
+# class UsersController < ApplicationController
+#   def new; end
+#
+#   def create
+#     user = User.new(user_params)
+#     if user.save
+#       session[:user_id] = user.id
+#       redirect_to '/profile'
+#       flash[:success] = "#{user.name}, You are now signed up!"
+#     else
+#       flash[:error] = "User could not be created: #{user.errors.full_messages.uniq.each {|msg| msg}}"
+#       redirect_to '/sign_up'
+#     end
+#   end
+#
+#   private
+#   def user_params
+#     binding.pry
+#     params.permit(:email, :username, :first_name, :last_name, :password, :password_confirmation)
+#   end
+# end
