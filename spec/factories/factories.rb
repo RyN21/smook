a66fc665c7c680f4d502db90921d16e6ed82f5f9
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-04-12 22:14:04 -0600

    I believe the factory is working

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "test-#{n.to_s.rjust(3, "0")}@sample.com" }
-    password { "Abc123!" }
+    username { "ryan21" }
+    first_name { "Ryan" }
+    password { "Abcd123!" }
   end
 end
